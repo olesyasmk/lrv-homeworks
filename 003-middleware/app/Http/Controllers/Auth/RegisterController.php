8e5833e1acf8a655bea6a7358bcdb99e29e5922f
olesyasmk <dynamicasync@gmail.com>
2026-02-13 12:58:04 +0500

    Fixes

--- 003-middleware/app/Http/Controllers/Auth/RegisterController.php
+++ 003-middleware/app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/groups';
 
     /**
      * Create a new controller instance.
