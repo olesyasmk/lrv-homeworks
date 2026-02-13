db7c8d14474b448a640ffa1a4f13bd5ccdf2abba
olesyasmk <dynamicasync@gmail.com>
2026-02-13 12:58:53 +0500

    Update web.php

--- 003-middleware/routes/web.php
+++ 003-middleware/routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('/groups');
+    }
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    Route::resource('groups', App\Http\Controllers\GroupController::class);
+    Route::resource('groups.students', App\Http\Controllers\StudentController::class);
+});
